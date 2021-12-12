be4347454ec0a46fa53215e00bf6ff8c4b0d5e97
j-goldrath <jonathan.goldrath@gmail.com>
2021-12-12 14:31:35 -0600

    fixed some compilation errors regarding links with rel="noopener noreferrer"

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,17 +19,17 @@ function Footer() {
                         <span className="text-muted">© 2021 J.Goldrath</span>
                     </div>
                     <ul className="nav col-md-4 justify-content-end list-unstyled d-flex no-bull">
-                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" href="email:jonathan.goldrath@gmail.com"><i className="bi bi-envelope" style={{ fontSize: '2rem' }} role="img" aria-label="email">
+                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" target="_blank" rel="noopener noreferrer" href="email:jonathan.goldrath@gmail.com"><i className="bi bi-envelope" style={{ fontSize: '2rem' }} role="img" aria-label="email">
                         </i></a></li>
-                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" href="tel:+18476875694"><i className="bi bi-telephone" style={{ fontSize: '2rem' }} role="img" aria-label="phone">
+                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" target="_blank" rel="noopener noreferrer" href="tel:+18476875694"><i className="bi bi-telephone" style={{ fontSize: '2rem' }} role="img" aria-label="phone">
                         </i></a></li>
-                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" target="_blank" href="https://github.com/j-goldrath"><i className="bi bi-github" style={{ fontSize: '2rem' }} role="img" aria-label="linkedin">
+                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" target="_blank" rel="noopener noreferrer" href="https://github.com/j-goldrath"><i className="bi bi-github" style={{ fontSize: '2rem' }} role="img" aria-label="linkedin">
                             <use xlinkHref="#github" />
                         </i></a></li>
-                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" target="_blank" href="https://linkedin.com"><i className="bi-linkedin" style={{ fontSize: '2rem' }} role="img" aria-label="linkedin">
+                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" target="_blank" rel="noopener noreferrer" href="https://linkedin.com"><i className="bi-linkedin" style={{ fontSize: '2rem' }} role="img" aria-label="linkedin">
                             <use xlinkHref="#linkedin" />
                         </i></a></li>
-                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" target="_blank" href="https://facebook.com"><i className="bi bi-facebook" style={{ fontSize: '2rem' }} role="img" aria-label="linkedin">
+                        <li className="ms-3 no-bull icon-grow"><a className="text-muted" target="_blank" rel="noopener noreferrer" href="https://facebook.com"><i className="bi bi-facebook" style={{ fontSize: '2rem' }} role="img" aria-label="linkedin">
                             <use xlinkHref="#facebook" />
                         </i></a></li>
                     </ul>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -11,11 +11,11 @@ function Portfolio() {
                 <div className="card img-card img-card-bg-1 shadow grow">
                     <div className="col s12">
                         <div className="card project-info-card align-items-center shadow">
-                            <a target="_blank" href="https://peaceful-beyond-09080.herokuapp.com/">
+                            <a target="_blank" rel="noopener noreferrer" href="https://peaceful-beyond-09080.herokuapp.com/">
                                 <h5 className="text-primary project-title text-center">Note Taker</h5>
                             </a>
                             <p className="project-description">Node.js | MySQL</p>
-                            <a target="_blank" className="project-github-icon" href="https://github.com/j-goldrath/note-taker"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
+                            <a target="_blank" rel="noopener noreferrer" className="project-github-icon" href="https://github.com/j-goldrath/note-taker"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
                         </div>
                     </div>
                 </div>
@@ -24,11 +24,11 @@ function Portfolio() {
                 <div className="card img-card img-card-bg-2 shadow grow">
                     <div className="col s12">
                         <div className="card project-info-card align-items-center shadow">
-                            <a target="_blank" href="https://j-goldrath.github.io/quiz-game/">
+                            <a target="_blank" rel="noopener noreferrer" href="https://j-goldrath.github.io/quiz-game/">
                                 <h5 className="text-primary project-title text-center">Simpsons Quiz Game</h5>
                             </a>
                             <p className="project-description text-center">JavaScript | HTML | CSS</p>
-                            <a target="_blank" className="project-github-icon" href="https://github.com/j-goldrath/quiz-game"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
+                            <a target="_blank" rel="noopener noreferrer" className="project-github-icon" href="https://github.com/j-goldrath/quiz-game"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
                         </div>
                     </div>
                 </div>
@@ -37,11 +37,11 @@ function Portfolio() {
                 <div className="card img-card img-card-bg-3 shadow grow">
                     <div className="col s12">
                         <div className="card project-info-card align-items-center shadow">
-                            <a target="_blank" href="https://hidden-springs-58589.herokuapp.com/">
+                            <a target="_blank" rel="noopener noreferrer" href="https://hidden-springs-58589.herokuapp.com/">
                                 <h5 className="text-primary project-title text-center">Fitness Tracker</h5>
                             </a>
                             <p className="project-description">Node.js | NoSQL</p>
-                            <a target="_blank" className="project-github-icon" href="https://github.com/j-goldrath/workout-tracker"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
+                            <a target="_blank" rel="noopener noreferrer" className="project-github-icon" href="https://github.com/j-goldrath/workout-tracker"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
                         </div>
                     </div>
                 </div>
@@ -50,11 +50,11 @@ function Portfolio() {
                 <div className="card img-card img-card-bg-4 shadow grow">
                     <div className="col s12">
                         <div className="card project-info-card align-items-center shadow">
-                            <a target="_blank" href="https://quiet-beach-13296.herokuapp.com/">
+                            <a target="_blank" rel="noopener noreferrer" href="https://quiet-beach-13296.herokuapp.com/">
                                 <h5 className="text-primary project-title text-center">Budget Tracker</h5>
                             </a>
                             <p className="project-description">Offline/Online PWA</p>
-                            <a target="_blank" className="project-github-icon" href="https://github.com/j-goldrath/budget-tracker"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
+                            <a target="_blank" rel="noopener noreferrer" className="project-github-icon" href="https://github.com/j-goldrath/budget-tracker"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
                         </div>
                     </div>
                 </div>
@@ -63,11 +63,11 @@ function Portfolio() {
                 <div className="card img-card img-card-bg-5 shadow grow">
                     <div className="col s12">
                         <div className="card project-info-card align-items-center shadow">
-                            <a target="_blank" href="https://afternoon-crag-37504.herokuapp.com/">
+                            <a target="_blank" rel="noopener noreferrer" href="https://afternoon-crag-37504.herokuapp.com/">
                                 <h5 className="text-primary project-title text-center">Tech Blog</h5>
                             </a>
                             <p className="project-description text-center">Node.js | MySQL | Handlebars</p>
-                            <a target="_blank" className="project-github-icon" href="https://github.com/j-goldrath/tech-blog"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
+                            <a target="_blank" rel="noopener noreferrer" className="project-github-icon" href="https://github.com/j-goldrath/tech-blog"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
                         </div>
                     </div>
                 </div>
@@ -76,11 +76,11 @@ function Portfolio() {
                 <div className="card img-card img-card-bg-6 shadow grow">
                     <div className="col s12">
                         <div className="card project-info-card align-items-center shadow">
-                            <a target="_blank" href="https://github.com/j-goldrath/team-profile-generator/blob/main/sample/team-profile-page-sample.html">
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/j-goldrath/team-profile-generator/blob/main/sample/team-profile-page-sample.html">
                                 <h5 className="text-primary project-title text-center">Team Profile Generator</h5>
                             </a>
                             <p className="project-description">Node.js</p>
-                            <a target="_blank" className="project-github-icon" href="https://github.com/j-goldrath/team-profile-generator"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
+                            <a target="_blank" rel="noopener noreferrer" className="project-github-icon" href="https://github.com/j-goldrath/team-profile-generator"><i style={{ fontSize: '2rem' }} className="bi bi-github" /></a>
                         </div>
                     </div>
                 </div>
