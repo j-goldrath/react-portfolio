0dfb17c10dfae67e2ac18a99319be689abe7222b
j-goldrath <jonathan.goldrath@gmail.com>
2021-12-12 14:54:46 -0600

    Updates

---
+++ precache-manifest.29c2c8463491b77f103bad6190615240.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597fc941dab55a8dc8f96bd4dd8defda",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3229cfc32a5e28210378",
+    "url": "/react-portfolio/static/css/main.e50decd9.chunk.css"
+  },
+  {
+    "revision": "38f60290af5811f95026",
+    "url": "/react-portfolio/static/js/2.2b890c90.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-portfolio/static/js/2.2b890c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c41787b8c405cdea85a",
+    "url": "/react-portfolio/static/js/3.46fb6ec3.chunk.js"
+  },
+  {
+    "revision": "3229cfc32a5e28210378",
+    "url": "/react-portfolio/static/js/main.8577a168.chunk.js"
+  },
+  {
+    "revision": "7d97314f28a56e3465a0",
+    "url": "/react-portfolio/static/js/runtime-main.f0b0b143.js"
+  },
+  {
+    "revision": "ad0f4a60bf05d9f1086ebf4353371929",
+    "url": "/react-portfolio/static/media/j-goldrath-profile-pic.ad0f4a60.png"
+  },
+  {
+    "revision": "cb43896056e759c029e1634dc6215e69",
+    "url": "/react-portfolio/static/media/super-stary-night.cb438960.jpg"
+  }
+]);
